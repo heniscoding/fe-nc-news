2f317457cc8495f586ad66082dfa34e2beff5cea
heniscoding <heniscoding@Ubuntu-22.04.3-Desktop.myguest.virtualbox.org>
2024-03-14 16:26:05 +0000

    Small style updates to text and colours

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -16,14 +16,11 @@ function ArticleCard({ article }) {
       <img className="article-card-img" src={article_img_url} alt="" />
       <div className="flex-container">
         <div className="article-card-title">{title}</div>
-        <div className="article-card-author">Author: {author}</div>
-        <div className="article-card-topic">Category: {topic}</div>
-        <div className="article-card-created">
-          Created: {formatAPIDate( created_at )}
-        </div>
-        <div className="article-card-votes">Votes: {votes}</div>
+        <div className="article-card-author">Created by <b>{author}</b> on <b>{formatAPIDate( created_at )}</b></div>
+        <div className="article-card-topic">Topic: <b>{topic}</b></div>
+        <div className="article-card-votes">Voted on <b>{votes}</b> times</div>
         <div className="article-card-comment-count">
-          Comments: {comment_count}
+          Commented on <b>{comment_count}</b> times
         </div>
       </div>
     </section>

b0d448e398d644b8a17597bde3374c1c6aac4ef4
heniscoding <heniscoding@Ubuntu-22.04.3-Desktop.myguest.virtualbox.org>
2024-03-14 15:54:34 +0000

    Completed task 12 - Error handling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; {new Date().getFullYear()} Northcoders News, built by Henry Alderslade</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,16 @@
 function Home() {
-    return <h2>Welcome to NC News</h2>;
+  return (
+    <div className="main-home-content">
+      <h2>Welcome to NC News</h2>
+      <p>Welcome to Northcoders News, your go-to destination for dynamic news aggregation, content rating, and engaging discussions. Powered by React, our web application offers a seamless user experience, allowing you to explore a wide range of articles, express your opinions through voting, and actively participate in community discussions.</p>
+      <p>
+      Our platform leverages a robust backend API, developed in a <a href="https://github.com/heniscoding/be-nc-news/blob/main/README.md" target="_blank">previous project</a>, to deliver a feature-rich interface that caters to your needs. Whether you're interested in the latest developments in technology, politics, entertainment, or any other topic, Northcoders News has you covered.
+      </p>
+      <p>
+      With intuitive browsing and filtering features, you can effortlessly navigate through articles, cast your votes, and discover new content that aligns with your interests. Join the conversation by posting comments, sharing your insights, and engaging with fellow users. Join us today and be part of our vibrant community!
+      </p>
+    </div>
+  );
 }
 
 export default Home;
